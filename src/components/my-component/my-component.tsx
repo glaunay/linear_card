98bced25144bc878d3d01970c400ee69d2d90079
sophielem <lematre.sophiegmail.com>
2019-07-10 16:28:53 +0200

    first commit

---
+++ src/components/my-component/my-component.tsx
@@ -0,0 +1,29 @@
+import { Component, Element, h } from '@stencil/core';
+
+@Component({
+  tag: 'linear-card',
+  styleUrl: 'my-component.css',
+  shadow: true
+})
+
+export class MyComponent {
+// *************************** PROPERTY & CONSTRUCTOR ***************************
+@Element() private element: HTMLElement;
+
+// *************************** LISTEN & EMIT ***************************
+
+
+
+// *************************** CLICK ***************************
+
+
+// *************************** DISPLAY ***************************
+  render() {
+    console.log("rendr called")
+
+
+    return ([
+
+      ]);
+  }
+}
