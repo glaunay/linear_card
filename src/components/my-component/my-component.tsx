d4102125b014fb20410d1d6e0c86d17f50996dcb
sophielem <lematre.sophiegmail.com>
2019-07-22 12:32:20 +0200

    bug size sgrnaBox

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -234,7 +234,7 @@ export class MyComponent {
         <div id="binSize">
           Bin size : <span style={{color:"rgb(239, 71, 111)", width:"35%"}}>{Math.ceil((this.coordGene["end"] - this.coordGene["start"])/(this.nb_step as unknown as number))}</span>
         </div>
-        <div style={{float:"right", width:"45%"}}>
+        <div style={{float:"right", width:"55%"}}>
           <strong> Sgrna box </strong>
           <div id="sgrnaBox"></div>
         </div>
